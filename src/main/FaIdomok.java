a12176dc98c8523c91da9698c98bfac72ddc9b53
Petrovai Asztrik Örs <asztrikpetrovai@gmail.com>
2023-11-29 13:49:15 +0100

    térfogat kiírása

--- src/main/FaIdomok.java
+++ src/main/FaIdomok.java
@@ -1,6 +1,5 @@
 package main;
 
-import idomok.FaIdom;
 import java.util.List;
 import idomok.FaIdom;
 import idomok.Gomb;
@@ -8,8 +7,6 @@ import idomok.Hasab;
 import java.util.ArrayList;
 
 public class FaIdomok {
-
-    private List<FaIdom> idomok;
 
     public FaIdomok() {
         run();
@@ -36,12 +33,15 @@ public class FaIdomok {
     }
     
     public void run(){
-        idomok = new ArrayList();
+        List<FaIdom> idomok = new ArrayList();
         idomok.add(new Gomb(3));
         idomok.add(new Gomb(1.5));
         idomok.add(new Hasab(3, 3, 2));
-        idomok.add(new Hasab(1, 0.5, 6));
-       
+        idomok.add(new Hasab(1, 1.5, 6));
+        
+        for (int i = 0; i < idomok.size(); i++) {
+            System.out.println(idomok.get(i).terfogat());
+        }
     }
     
 }
