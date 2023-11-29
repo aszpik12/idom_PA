9cf837ce69a58c5b7752fc0d6622ec7d4a9f058f
Petrovai Asztrik Örs <asztrikpetrovai@gmail.com>
2023-11-29 12:37:43 +0100

    kezdet

---
+++ src/idom_pa/Idom_PA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package idom_pa;
+
+/**
+ *
+ * @author PetrovaiAsztrikÖrs(S
+ */
+public class Idom_PA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
